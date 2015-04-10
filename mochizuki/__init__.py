7cdb618caffc3d27e1c0aaf7dc78319e255cd052
Kenny Do <chinesedewey@gmail.com>
2015-04-10 02:04:47 -0400

    update client handler to echo received data

--- mochizuki/__init__.py
+++ mochizuki/__init__.py
@@ -1,5 +1,7 @@
 import asyncio
 import logging
+import socket
+
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -7,14 +9,19 @@ logger = logging.getLogger(__name__)
 
 class IRCClientProtocol(asyncio.Protocol):
     def connection_made(self, transport):
-        self._transport = transport
+        self._socket = transport.get_extra_info('socket')
+        logger.info("Connection made to socket: %r", self._socket)
 
     def connection_lost(self, exc=None):
-        logger.info("Connection lost!")
+        logger.info("Connection lost to socket: %r", self._socket)
 
     def data_received(self, data):
         print(type(data))
         print(repr(data))
+        try:
+            self._socket.send(data)
+        except socket.error:
+            logger.exception("Unable to send data to socket %r", self._socket)
 
 
 class Mochizuki(object):
@@ -23,15 +30,14 @@ class Mochizuki(object):
     def __init__(self, host='127.0.0.1', port=6667):
         self.host = host
         self.port = port
+
+        # handle the async magic
         self._loop = asyncio.get_event_loop()
-        self._server_coro = self._loop.create_server(self.client_protocol_factory, self.host, self.port)
+        self._server_coro = self._loop.create_server(
+            self.client_protocol_factory, self.host, self.port)
         self._server = self._loop.run_until_complete(self._server_coro)
 
-    def log_sockets(self):
-        logger.info(repr(self._server.sockets))
-
     def run(self):
-        self._loop.call_later(4, self.log_sockets)
         try:
             self._loop.run_forever()
         except KeyboardInterrupt:
@@ -41,6 +47,7 @@ class Mochizuki(object):
         self._server.close()
         self._loop.run_until_complete(self._server.wait_closed())
         self._loop.close()
+        logger.info("Server stopped.")
 
 
 if __name__ == "__main__":
