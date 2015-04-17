e4fc63ee0ddf1bdd3709f823a4b5b5eeaca50ace
Kenny Do <chinesedewey@gmail.com>
2015-04-17 15:18:18 -0700

    add prefix property to IRCUser

--- mochizuki/users.py
+++ mochizuki/users.py
@@ -8,3 +8,15 @@ class IRCUser(object):
         self.username = None
         self.hostname = None
         self.realname = None
+
+    @property
+    def prefix(self):
+        """The prefix send in PRIVMSG and other messages to indicate the
+        origin of the message
+        """
+
+        return "{nickname}!{username}@{hostname}".format(
+            nickname=self.nickname,
+            username=self.username,
+            hostname=self.hostname,
+        )
